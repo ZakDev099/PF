8c70f979dcd5371766576180e112730f16fd4d74
Zak Dyer <30128282@tafe.wa.edu.au>
2025-07-24 10:48:59 +0800

    merge

---
+++ NewProgram/Calc.cs
@@ -0,0 +1,12 @@
+﻿namespace NewProgram;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int value1 = Console.ReadLine();
+        int value2 = Console.ReadLine();
+        double result = value1 * value 2;
+        Console.WriteLine(result);
+    }
+}
