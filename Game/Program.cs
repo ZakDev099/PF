0fb38ef69eaa313d4e0b9333de815ebbe4c91abe
Zak Dyer <30128282@tafe.wa.edu.au>
2025-07-26 14:19:54 +0800

    added stuff

---
+++ Game/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Game;
+
+class Player(string playerName)
+{
+    string playerName;
+    int level = 0;
+}
+
+class Weapon
+{
+
+}
+
+class Combat
+{
+
+}
+
+class Reward
+{
+
+}
+
+class UserInput
+{
+    static int numSelect
+    {
+        
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your name");
+        Player1 = new Player(Console.ReadLine());
+        Console.WriteLine("Welcome " + Player1.playerName + "!\nPlease select a weapon:\n\n1. Copper Broadsword\n2.");
+    }
+}
