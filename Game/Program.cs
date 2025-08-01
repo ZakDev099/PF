b7fd2d9a87ee6e37b422512cc94e14377c87fc08
Zak Dyer <30128282@tafe.wa.edu.au>
2025-08-02 00:18:48 +0800

    added combat (incomplete)

--- Game/Program.cs
+++ Game/Program.cs
@@ -20,13 +20,16 @@ class Player(string name)
     }
 }
 
-class Enemy(string name, int health, int attack)
+class Enemy(string name, int level, int health, int attack)
 {
     public string enemyName = name;
+    public int enemyLevel = level;
     public int maxHealth = health;
     public int currentHealth = health;
     public int enemyAttack = attack;
+    public int goldReward = 
 }
+
 class Weapon(string name, int damage, int accuracy, int block)
 {
     public string weaponName = name;
@@ -34,7 +37,8 @@ class Weapon(string name, int damage, int accuracy, int block)
     public int weaponAccuracy = accuracy;
     public int weaponBlock = block;
 }
-// Add list as return
+
+// To Do: Add list as return
 class ObjectLoader
 {
     public static SortedDictionary<string, Weapon> LoadWeapons()
@@ -48,11 +52,41 @@ class ObjectLoader
         };
         return weapons;
     }
+    public static sortedDictionary<string, Enemy> LoadEnemies()
+    {
+        Enemy slime = new("Slime", 1, 4, 1);
+        Enemy goblin = new("Goblin", 2, 6, 3);
+    }
 }
 
-class Battle
+class Gameplay
 {
-    
+    public static void CombatRound(Enemy activeEnemy)
+    {
+        int enemyDamage = activeEnemy.enemyAttack;
+        int playerDamage = Player1.equippedWeapon.weaponDamage;
+        Console.PrintLine($"UH OH! a {activeEnemy} appeared!");
+        while (activeEnemy.currentHealth > 0 || Player1.currentHealth > 0)
+        {
+            Console.PrintLine("1. Attack\n");
+            Console.PrintLine("Please select an action: ");
+            int userInput = InputHandler.ToInt([1]);
+            if (userInput == 1)
+            {
+                activeEnemy.currentHealth -= playerDamage;
+            }
+            if (activeEnemy.currentHealth > 0)
+            {
+                Player1.currentHealth -= enemyDamage;
+            }
+        }
+        if (activeEnemy.currentHealth <= 0)
+        {
+            Console.WriteLine($"{activeEnemy} has died!");
+            Player1.gold += activeEnemy.enemyLevel;
+
+        }
+    }
 }
 
 class InputHandler
@@ -85,12 +119,24 @@ class InputHandler
     }
 }
 
+/*class Tools
+{
+    public static int Randomizer(int inputNumber)
+    {
+        
+    }
+}
+*/
+
 class Program
 {
     static void Main(string[] args)
     {
+        random = new()
         Console.WriteLine("Loading Weapons...\n");
         var allWeapons = ObjectLoader.LoadWeapons();
+        Console.WriteLine("Loading Enemies...\n");
+        var allEnemies = ObjectLoader.LoadEnemies();
         Console.WriteLine("Please enter your name");
         var Player1 = new Player(Console.ReadLine());
         Console.WriteLine($"Welcome {Player1.playerName}!");
@@ -104,6 +150,6 @@ class Program
         {
             Player1.GiveWeapon(allWeapons["sling"]);
         }
-        
+
     }
 }
