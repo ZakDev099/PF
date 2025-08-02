a90534c16b45404a728b7493c0e71bf8997b81dd
Zak Dyer <30128282@tafe.wa.edu.au>
2025-08-02 22:56:23 +0800

    several improvements to menu

--- Game/Program.cs
+++ Game/Program.cs
@@ -2,12 +2,13 @@
 using System.Diagnostics.Contracts;
 using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
 
 namespace Game;
 
-class Player(string name)
+class Player()
 {
-    public string playerName = name;
+    public string playerName = "player";
     public int level = 0;
     public int gold = 0;
     public Weapon ?equippedWeapon;
@@ -20,20 +21,22 @@ class Player(string name)
     }
 }
 
-class Enemy(string name, int level, int health, int attack)
+class Enemy(string name, int level, int health, int attack, int rarity)
 {
     public string enemyName = name;
     public int enemyLevel = level;
     public int maxHealth = health;
     public int currentHealth = health;
     public int enemyAttack = attack;
-    public int goldReward = level*10;
+    public int enemyRarity = rarity;
+    public int goldReward = level * 10;
 }
 
-class Weapon(string name, int damage)
+class Weapon(string name, int damage, int rarity)
 {
     public string weaponName = name;
     public int weaponDamage = damage;
+    public int weaponRarity = rarity;
 }
 
 // To Do: Add list as return
@@ -41,8 +44,9 @@ class ObjectLoader
 {
     public static SortedDictionary<string, Weapon> LoadWeapons()
     {
-        Weapon woodenClub = new("Wooden Club", 1);
-        Weapon sling = new("Sling", 1);
+        Console.WriteLine("Loading Weapons...");
+        Weapon woodenClub = new("Wooden Club", 1, 1);
+        Weapon sling = new("Sling", 1, 1);
         var weapons = new SortedDictionary<string, Weapon>
         {
             {"woodenClub", woodenClub},
@@ -52,8 +56,9 @@ class ObjectLoader
     }
     public static SortedDictionary<string, Enemy> LoadEnemies()
     {
-        Enemy slime = new("Slime", 1, 4, 1);
-        Enemy goblin = new("Goblin", 2, 6, 3);
+        Console.WriteLine("Loading Enemies...\n");
+        Enemy slime = new("Slime", 1, 4, 1, 1);
+        Enemy goblin = new("Goblin", 2, 6, 3, 1);
         var enemies = new SortedDictionary<string, Enemy>
         {
             {"slime", slime},
@@ -63,28 +68,54 @@ class ObjectLoader
     }
 }
 
-class Play
+class PlayGame(Player player1)
 {
-    public static void CombatRound(Player activePlayer, Enemy activeEnemy, Random random)
+    public Player player1 = player1;
+    public void StartGame(SortedDictionary<string, Weapon> allWeapons)
+    {
+        Console.WriteLine("Please enter your name");
+        Console.WriteLine($"Welcome {player1.playerName}!");
+        Console.WriteLine
+        (
+            "1.Wooden Club",
+            "2.Sling",
+            "Please select a starting weapon: "
+        );
+        int userInput = InputHandler.ToInt([1, 2]);
+        if (userInput == 1)
+        {
+            player1.GiveWeapon(allWeapons["woodenClub"]);
+        }
+        else
+        {
+            player1.GiveWeapon(allWeapons["sling"]);
+        }
+    }
+    
+    public void CombatRound(Enemy activeEnemy, Random random)
     {
         int enemyDamage = activeEnemy.enemyAttack;
-        int playerDamage = activePlayer.equippedWeapon.weaponDamage;
+        int playerDamage = player1.equippedWeapon.weaponDamage;
+
         Console.WriteLine($"UH OH! a {activeEnemy.enemyName} appeared!");
-        while (activeEnemy.currentHealth > 0 && activePlayer.currentHealth > 0)
+        while (activeEnemy.currentHealth > 0 && player1.currentHealth > 0)
         {
-            Console.WriteLine("1. Attack\n");
-            Console.WriteLine("Please select an action: ");
+            Console.WriteLine
+            (
+                "1. Attack",
+                "Please select an action: "
+            );
             int userInput = InputHandler.ToInt([1]);
             if (userInput == 1)
             {
-                int damageDealt = random.Next(playerDamage - 1, playerDamage + 1);
+                int damageDealt = random.Next(playerDamage - 1, playerDamage + 2);
                 activeEnemy.currentHealth -= damageDealt;
                 Console.WriteLine($"You hit {activeEnemy.enemyName}, dealing {damageDealt} damage!");
             }
             if (activeEnemy.currentHealth > 0)
             {
-                int damageTaken = random.Next(enemyDamage - 1, enemyDamage + 1);
-                activePlayer.currentHealth -= damageTaken;
+                int damageTaken = random.Next(enemyDamage - 1, enemyDamage + 2);
+                player1.currentHealth -= damageTaken;
                 Console.WriteLine($"{activeEnemy.enemyName} hits you, dealing {damageTaken} damage!");
             }
         }
@@ -92,8 +123,47 @@ class Play
         {
             Console.WriteLine($"{activeEnemy.enemyName} has died!");
             int goldRecieved = random.Next(activeEnemy.goldReward / 2, activeEnemy.goldReward * 2);
-            activePlayer.gold += goldRecieved;
+            player1.gold += goldRecieved;
             Console.WriteLine($"You recieved {goldRecieved} gold!");
+        }
+    }
+}
+
+class Menu
+{
+    public static void MainMenu
+    (
+        Random globalRandom,
+        SortedDictionary<string, Weapon> allWeapons,
+        SortedDictionary<string, Enemy> allEnemies,
+        Player player1,
+        PlayGame Play,
+        bool runProgram
+    )
+    {
+        Console.WriteLine
+        (
+            "1. Fight",
+            "2. Shop",
+            "3. My Character",
+            "4. Quit Game"
+        );
+        var userInput = InputHandler.ToInt([1, 2, 3, 4]);
+        if (userInput == 1)
+        {
+            //Play.CombatRound()...
+        }
+        else if (userInput == 2)
+        {
+            //Shop code goes here...
+        }
+        else if (userInput == 3)
+        {
+            //Show character inventory, stats, etc...
+        }
+        else
+        {
+            //Are you sure you want to quit?...
         }
     }
 }
@@ -133,23 +203,16 @@ class Program
     static void Main(string[] args)
     {
         Random globalRandom = new();
-        Console.WriteLine("Loading Weapons...");
         var allWeapons = ObjectLoader.LoadWeapons();
-        Console.WriteLine("Loading Enemies...\n");
         var allEnemies = ObjectLoader.LoadEnemies();
-        Console.WriteLine("Please enter your name");
-        var player1 = new Player(Console.ReadLine());
-        Console.WriteLine($"Welcome {player1.playerName}!");
-        Console.WriteLine("1.Wooden Club\n2.Sling\n\nPlease select a starting weapon: ");
-        int userInput = InputHandler.ToInt([1, 2]);
-        if (userInput == 1)
+        var player1 = new Player();
+        PlayGame Play = new(player1);
+        Play.StartGame(allWeapons);
+        bool runProgram = true;
+        while (runProgram)
         {
-            player1.GiveWeapon(allWeapons["woodenClub"]);
+            Menu.MainMenu(globalRandom, allWeapons, allEnemies, player1, Play, runProgram);
         }
-        else
-        {
-            player1.GiveWeapon(allWeapons["sling"]);
-        }
-        Play.CombatRound(player1, allEnemies["slime"], globalRandom);
+        //Play.CombatRound(allEnemies["slime"], globalRandom);
     }
 }
