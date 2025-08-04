af19ae07f0a3e00425f6fb9f01924d41e40614e5
Zak Dyer <30128282@tafe.wa.edu.au>
2025-08-04 19:08:14 +0800

    Added random enemies
    + fixed gold reward

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.Contracts;
+using System.Dynamic;
 using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualBasic;
@@ -8,35 +10,60 @@ namespace Game;
 
 class Player()
 {
-    public string playerName = "player";
-    public int level = 0;
-    public int gold = 0;
-    public Weapon ?equippedWeapon;
-    public int maxHealth = 10;
-    public int currentHealth = 10;
+    public string PlayerName = "player";
+    public int PlayerLevel = 1;
+    public int Gold = 0;
+    public Weapon ?EquippedWeapon;
+    public int MaxHealth = 10;
+    public int CurrentHealth = 10;
     public void GiveWeapon(Weapon weaponReward)
     {
-        equippedWeapon = weaponReward;
-        Console.WriteLine($"You have equipped {equippedWeapon.weaponName}!");
+        EquippedWeapon = weaponReward;
+        Console.WriteLine($"You have equipped {EquippedWeapon.WeaponName}!");
     }
 }
 
-class Enemy(string name, int level, int health, int attack, int rarity)
+class Enemy(string name, int level, int health, int attack, int spawnChance, int gold)
 {
-    public string enemyName = name;
-    public int enemyLevel = level;
-    public int maxHealth = health;
-    public int currentHealth = health;
-    public int enemyAttack = attack;
-    public int enemyRarity = rarity;
-    public int goldReward = level * 10;
+    public string EnemyName = name;
+    public int EnemyLevel = level;
+    public int MaxHealth = health;
+    public int CurrentHealth = health;
+    public int EnemyAttack = attack;
+    public int SpawnChance = spawnChance;
+    public int GoldReward = gold;
+
+    public static Enemy GenerateEnemy(SortedDictionary<string, Enemy> allEnemies, int PlayerLevel, Random globalRandom)
+    {
+        int randomCap = 0;
+        SortedDictionary<int, Enemy> enemyPool = new();
+        foreach (Enemy enemy in allEnemies.Values)
+        {
+            if (enemy.EnemyLevel == PlayerLevel)
+            {
+                enemyPool.Add(enemy.SpawnChance, enemy);
+                randomCap += enemy.SpawnChance;
+            }
+        }
+        int randomSelection = globalRandom.Next(1, randomCap + 1);
+        int position = 0;
+        foreach (KeyValuePair<int, Enemy> enemy in enemyPool)
+        {
+            position += enemy.Key;
+            if (randomSelection <= position)
+            {
+                return (Enemy)enemy.Value.MemberwiseClone();
+            }
+        }
+        return (Enemy)allEnemies["voidSlime"].MemberwiseClone();
+    }
 }
 
 class Weapon(string name, int damage, int rarity)
 {
-    public string weaponName = name;
-    public int weaponDamage = damage;
-    public int weaponRarity = rarity;
+    public string WeaponName = name;
+    public int WeaponDamage = damage;
+    public int WeaponRarity = rarity;
 }
 
 // To Do: Add list as return
@@ -57,12 +84,20 @@ class ObjectLoader
     public static SortedDictionary<string, Enemy> LoadEnemies()
     {
         Console.WriteLine("Loading Enemies...\n");
-        Enemy slime = new("Slime", 1, 4, 1, 1);
-        Enemy goblin = new("Goblin", 2, 6, 3, 1);
+        Enemy voidSlime = new("Void Slime", 1, 1, 1, 0, 0);
+        Enemy greenSlime = new("Green Slime", 1, 3, 1, 10, 10);
+        Enemy blueSlime = new("Blue Slime", 1, 6, 1, 8, 15);
+        Enemy redSlime = new("Red Slime", 1, 10, 1, 6, 20);
+        Enemy iridescentSlime = new("Iridescent Slime", 1, 10, 1, 1, 100);
+        Enemy goblin = new("Goblin", 2, 6, 3, 20, 50);
         var enemies = new SortedDictionary<string, Enemy>
         {
-            {"slime", slime},
-            {"goblin", goblin}
+            {"voidSlime", voidSlime},
+            {"greenSlime", greenSlime},
+            {"blueSlime", blueSlime},
+            {"redSlime", redSlime},
+            {"iridescentSlime", iridescentSlime},
+            { "goblin", goblin}
         };
         return enemies;
     }
@@ -71,14 +106,16 @@ class ObjectLoader
 class PlayGame(Player player1)
 {
     public Player player1 = player1;
+
     public void StartGame(SortedDictionary<string, Weapon> allWeapons)
     {
         Console.WriteLine("Please enter your name");
-        Console.WriteLine($"Welcome {player1.playerName}!");
+        player1.PlayerName =Console.ReadLine();
+        Console.WriteLine($"Welcome {player1.PlayerName}!\n");
         Console.WriteLine
         (
-            "1.Wooden Club",
-            "2.Sling",
+            "1.Wooden Club\n"+
+            "2.Sling\n"+
             "Please select a starting weapon: "
         );
         int userInput = InputHandler.ToInt([1, 2]);
@@ -91,41 +128,43 @@ class PlayGame(Player player1)
             player1.GiveWeapon(allWeapons["sling"]);
         }
     }
-    
     public void CombatRound(Enemy activeEnemy, Random random)
     {
-        int enemyDamage = activeEnemy.enemyAttack;
-        int playerDamage = player1.equippedWeapon.weaponDamage;
+        int enemyDamage = activeEnemy.EnemyAttack;
+        int playerDamage = player1.EquippedWeapon.WeaponDamage;
 
-        Console.WriteLine($"UH OH! a {activeEnemy.enemyName} appeared!");
-        while (activeEnemy.currentHealth > 0 && player1.currentHealth > 0)
+        Console.WriteLine($"\nUH OH! a {activeEnemy.EnemyName} appeared!");
+        while (activeEnemy.CurrentHealth > 0 && player1.CurrentHealth > 0)
         {
             Console.WriteLine
             (
-                "1. Attack",
+                "1. Attack\n" +
                 "Please select an action: "
             );
             int userInput = InputHandler.ToInt([1]);
             if (userInput == 1)
             {
                 int damageDealt = random.Next(playerDamage - 1, playerDamage + 2);
-                activeEnemy.currentHealth -= damageDealt;
-                Console.WriteLine($"You hit {activeEnemy.enemyName}, dealing {damageDealt} damage!");
+                activeEnemy.CurrentHealth -= damageDealt;
+                Console.WriteLine($"\nYou hit {activeEnemy.EnemyName}, dealing {damageDealt} damage!");
             }
-            if (activeEnemy.currentHealth > 0)
+            if (activeEnemy.CurrentHealth > 0)
             {
                 int damageTaken = random.Next(enemyDamage - 1, enemyDamage + 2);
-                player1.currentHealth -= damageTaken;
-                Console.WriteLine($"{activeEnemy.enemyName} hits you, dealing {damageTaken} damage!");
+                player1.CurrentHealth -= damageTaken;
+                Console.WriteLine($"{activeEnemy.EnemyName} hits you, dealing {damageTaken} damage!");
             }
         }
-        if (activeEnemy.currentHealth <= 0)
+        if (activeEnemy.CurrentHealth <= 0)
         {
-            Console.WriteLine($"{activeEnemy.enemyName} has died!");
-            int goldRecieved = random.Next(activeEnemy.goldReward / 2, activeEnemy.goldReward * 2);
-            player1.gold += goldRecieved;
-            Console.WriteLine($"You recieved {goldRecieved} gold!");
+            Console.WriteLine($"{activeEnemy.EnemyName} has died!");
+            int GoldRecieved = random.Next(activeEnemy.GoldReward / 2, activeEnemy.GoldReward * 2);
+            player1.Gold += GoldRecieved;
+            Console.WriteLine($"You recieved {GoldRecieved} gold!");
+            Console.WriteLine($"You have {player1.Gold} gold!");
+            return;
         }
+        //WRITE CONDITION FOR CHARACTER DEATH...
     }
 }
 
@@ -137,21 +176,22 @@ class Menu
         SortedDictionary<string, Weapon> allWeapons,
         SortedDictionary<string, Enemy> allEnemies,
         Player player1,
-        PlayGame Play,
+        PlayGame play,
         bool runProgram
     )
     {
         Console.WriteLine
         (
-            "1. Fight",
-            "2. Shop",
-            "3. My Character",
+            "\n1. Fight\n"+
+            "2. Shop\n"+
+            "3. My Character\n"+
             "4. Quit Game"
         );
         var userInput = InputHandler.ToInt([1, 2, 3, 4]);
         if (userInput == 1)
         {
-            //Play.CombatRound()...
+            Enemy combatEnemy = Enemy.GenerateEnemy(allEnemies, player1.PlayerLevel, globalRandom);
+            play.CombatRound(combatEnemy, globalRandom);
         }
         else if (userInput == 2)
         {
@@ -206,12 +246,12 @@ class Program
         var allWeapons = ObjectLoader.LoadWeapons();
         var allEnemies = ObjectLoader.LoadEnemies();
         var player1 = new Player();
-        PlayGame Play = new(player1);
-        Play.StartGame(allWeapons);
+        PlayGame play = new(player1);
+        play.StartGame(allWeapons);
         bool runProgram = true;
         while (runProgram)
         {
-            Menu.MainMenu(globalRandom, allWeapons, allEnemies, player1, Play, runProgram);
+            Menu.MainMenu(globalRandom, allWeapons, allEnemies, player1, play, runProgram);
         }
         //Play.CombatRound(allEnemies["slime"], globalRandom);
     }
--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab290621030155959406a2228fe6cac339ef9cce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90534c16b45404a728b7493c0e71bf8997b81dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
